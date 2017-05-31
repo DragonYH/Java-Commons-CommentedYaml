b583f29305b3ca602b9ac9f3f6aa6b3cb08c6986
聪聪 <178666380@qq.com>
2017-05-31 16:35:13 +0800

    Serializable对象的序列化与反序列化

--- src/main/java/cc/commons/commentedyaml/CommentedConstructor.java
+++ src/main/java/cc/commons/commentedyaml/CommentedConstructor.java
@@ -1,60 +1,85 @@
 package cc.commons.commentedyaml;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.LinkedHashSet;
 import java.util.Map;
 
 import org.yaml.snakeyaml.constructor.Constructor;
 import org.yaml.snakeyaml.error.YAMLException;
+import org.yaml.snakeyaml.introspector.GenericProperty;
+import org.yaml.snakeyaml.introspector.Property;
 import org.yaml.snakeyaml.nodes.Node;
 import org.yaml.snakeyaml.nodes.Tag;
 
+import cc.commons.commentedyaml.serialize.conver.DataConvert;
+
 public class CommentedConstructor extends Constructor{
+
+    /** GenericProperty的genType值域,用于数据类型转换 */
+    protected static Field field_GenericProperty_genType;
+
+    static{
+        try{
+            field_GenericProperty_genType=GenericProperty.class.getDeclaredField("genType");
+            field_GenericProperty_genType.setAccessible(true);
+        }catch(NoSuchFieldException|SecurityException exp){
+            throw new IllegalStateException(exp.getLocalizedMessage(),exp);
+        }
+
+    }
+
+    /** 当前被使用的数据转换类实例 */
+    protected LinkedHashSet<DataConvert> mDataConvert=new LinkedHashSet<>();
 
     public CommentedConstructor(){
         this.yamlConstructors.put(Tag.MAP,new ConstructCustomObject());
     }
 
+    /**
+     * 获取正在使用的数据转换类实例
+     * <p>
+     * 数据转换类实例的顺序十分重要<br>
+     * key为null为默认的数据转换类实例
+     * </p>
+     * 
+     * @return 数据转换类实例
+     */
+    public LinkedHashSet<DataConvert> getDataConvert(){
+        return this.mDataConvert;
+    }
+
     public class ConstructCustomObject extends ConstructYamlMap{
 
         @Override
-        public Object construct(Node node){
-            if(node.isTwoStepsConstruction()){
-                throw new YAMLException("Unexpected referential mapping structure. Node: "+node);
+        public Object construct(Node pNode){
+            if(pNode.isTwoStepsConstruction()){
+                throw new YAMLException("Unexpected referential mapping structure. Node: "+pNode);
             }
 
-            Map<?,?> tRawData=(Map<?,?>)super.construct(node);
+            Map<?,?> tRawData=(Map<?,?>)super.construct(pNode);
             Object tObj=tRawData.get(CommentedRepresenter.SerializableMark);
             if(tObj==null){
                 return tRawData;
             }
 
             try{
-                Class<?> tClazz=Class.forName(String.valueOf(tObj));
+                String tTypeStr=String.valueOf(tObj);
+                Class<?> tClazz=Class.forName(tTypeStr);
                 java.lang.reflect.Constructor<?> tConstruct=tClazz.getDeclaredConstructor();
                 tConstruct.setAccessible(true);
                 tObj=tConstruct.newInstance();
-
-                for(Map.Entry<?,?> sEntry : tRawData.entrySet()){
-                    String tFieldName=String.valueOf(sEntry.getKey());
-                    if(tFieldName.equals(CommentedRepresenter.SerializableMark))
+                for(Property sProp : CommentedConstructor.this.getPropertyUtils().getProperties(tClazz)){
+                    if(!sProp.isWritable()||!tRawData.containsKey(sProp.getName()))
                         continue;
 
-                    Field tField=null;
-                    Class<?> tSClazz=tClazz;
-                    while(tField==null&&tSClazz!=null&&tSClazz!=Object.class){
-                        for(Field sField : tSClazz.getDeclaredFields()){
-                            if(sField.getName().equals(tFieldName)){
-                                tField=sField;
-                                break;
-                            }
-                        }
-                    }
-                    if(tField==null)
-                        continue;
-
-                    tField.setAccessible(true);
-                    tField.set(tObj,sEntry.getValue());
+                    Object tValue=DataConvert.convertData(
+                            tRawData.get(sProp.getName()),
+                            sProp.getType(),
+                            (Type)field_GenericProperty_genType.get(sProp));
+                    sProp.set(tObj,tValue);
                 }
+
                 return tObj;
             }catch(Throwable exp){
                 throw new YAMLException("Could not deserialize object",exp);
--- src/main/java/cc/commons/commentedyaml/CommentedRepresenter.java
+++ src/main/java/cc/commons/commentedyaml/CommentedRepresenter.java
@@ -1,8 +1,14 @@
 package cc.commons.commentedyaml;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import org.yaml.snakeyaml.nodes.MappingNode;
 import org.yaml.snakeyaml.nodes.Node;
+import org.yaml.snakeyaml.nodes.NodeTuple;
+import org.yaml.snakeyaml.nodes.Tag;
 import org.yaml.snakeyaml.representer.Represent;
 import org.yaml.snakeyaml.representer.Representer;
 
@@ -10,12 +16,13 @@ public class CommentedRepresenter extends Representer{
 
     /** 实现Serializable接口的类的类型标记 */
     public static String SerializableMark="===";
-    /** 实现MapSerialize接口的类的类型标记*/
+    /** 实现MapSerialize接口的类的类型标记 */
     public static String MapSerializeMark="==";
-    
+
     public CommentedRepresenter(){
         this.multiRepresenters.put(CommentedSection.class,new CommentedSectionRepresent());
         this.multiRepresenters.put(CommentedValue.class,new CommentedValueRepresent());
+        this.multiRepresenters.put(Serializable.class,new RepresentSerializable());
     }
 
     public Represent getNullRepresent(){
@@ -30,7 +37,27 @@ public class CommentedRepresenter extends Representer{
         return this.multiRepresenters;
     }
 
+    public class RepresentSerializable extends RepresentJavaBean{
+
+        @Override
+        public Node representData(Object pData){
+            MappingNode tNode=(MappingNode)super.representData(pData);
+            tNode.setTag(Tag.MAP);
+
+            Represent tStringRepresent=CommentedRepresenter.this.getRepresents().get(String.class);
+            List<NodeTuple> tWarpFields=new ArrayList<>();
+            tWarpFields.add(new NodeTuple(
+                    tStringRepresent.representData(SerializableMark),
+                    tStringRepresent.representData(pData.getClass().getName())));
+            tWarpFields.addAll(tNode.getValue());
+            tNode.setValue(tWarpFields);
+
+            return tNode;
+        }
+    }
+
     public class CommentedSectionRepresent extends RepresentMap{
+
         @Override
         public Node representData(Object pData){
             return super.representData(((CommentedSection)pData).getValues(false));
